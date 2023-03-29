380ea2bf3e6aca38867ab14a088b3d5d8d8a2e41
domitron123 <dom-townshend@live.nmit.ac.nz>
2023-03-30 09:42:30 +1300

    working on grid layout - and mine generation

--- msTEST.js
+++ msTEST.js
@@ -1,34 +1,60 @@
-myArr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]; // 3x3 array
+// Define the array with all elements initialized to 0
+let myArr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
-
-// THIS IS WEIRD AND NOT THE SAME 1/3 CHANCE 
-mineX = Math.floor(Math.random() * 10);
-for(let i = mineX; i < 1 || i > 3; i--) {
-    if(i < 1) {
-        mineX += 1;
-        break;
-    }
-    else if(i > 3) {
-        mineX -= 1;
-    }
-    else if(i === 3) {
-        break;
-    }
-}
-    
-console.log(mine)
-
+// Define an empty string to store the grid
 let grid = "";
 
+// Set the maximum and minimum values for the random number generator
+let max = myArr.length;
+let min = 1;
+
+// Generate a random integer between min and max (inclusive)
+let mine = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Print the random number
+console.log(mine);
+
+
+// Loop through the rows of the array
 for(let i = 0; i < myArr.length; i++) {
+
+    // If the grid is not empty, add a new line character to separate the rows
     if(grid != "") {
         grid += "\n";
+        
     }
-    
-    for(let j = 0; j <myArr[i].length; j++) {
+
+    grid += "X:" + (i + 1);
+
+    // Loop through the columns of the array
+    for(let j = 0; j < myArr[i].length; j++) {
+        // Add a space and a 0 to the grid for each element in the row
+        // if(i === 0) {
+        //     grid += "Y"
+        //     grid += "\n"
+           
+        // }
         grid += " 0";
+        console.log(j)
         
     }
 }
 
+// Print the grid
 console.log(grid);
+
+
+// THIS IS WEIRD AND NOT THE SAME 1/3 CHANCE 
+// mineX = Math.floor(Math.random() * 10);
+// for(let i = mineX; i < 1 || i > 3; i--) {
+//     if(i < 1) {
+//         mineX += 1;
+//         break;
+//     }
+//     else if(i > 3) {
+//         mineX -= 1;
+//     }
+//     else if(i === 3) {
+//         break;
+//     }
+// }
