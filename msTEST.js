c329aef59d1f5aeeb790b174ec9f85dd678bb68f
domitron123 <dom-townshend@live.nmit.ac.nz>
2023-04-04 18:30:16 +1200

    Refined code, fixed some spelling, added a coordinate limit for user input with error message

--- msTEST.js
+++ msTEST.js
@@ -3,8 +3,8 @@ const readline = require('readline');
 
 // Initialize empty arrays and strings for grid generation
 let gridArr = [];
-let nested = [];
 let grid = "";
+gameOver = false;
 
 function gridGen(x, y) {
     // for each y value, push that many subarrays within gridArr !ROWS!
@@ -21,6 +21,7 @@ function gridGen(x, y) {
 
 // gridUpate function that converts the girdArr to a tileable string 'grid'
 function gridUpdate() {
+    // Reset the grid string to nothing so it doesn't keep adding to the previous grid
     grid = "";
     // loop for each subarray in gridArr (the rows)
     for(let i = 0; i < gridArr.length; i++) {
@@ -30,20 +31,19 @@ function gridUpdate() {
         }
         // for the length of each subarray (the colums) add a cell
         for(let j = 0; j < gridArr[i].length; j++) {
+            //ANCHOR - if the value of the cell is 0, add a 0 to the grid, else add a 1
             if(gridArr[i][j] == 0) {
                 grid += "[" + 0 + "]";
             } else {
-                grid += "[" + 1 + "]";
+                grid += "[" + "-" + "]";
             }
         }
     }
-    console.log("---------------------")
     console.log(grid)
 }
 
 gridGen(3, 3);
 gridUpdate()
-
 
 // create a user input with terminal 
 const rl = readline.createInterface({
@@ -51,18 +51,28 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-// ask the user for desired grid chords to 'clear'
-rl.question('chords x: ', (chordX) => {
-    rl.question('chords y: ', (chordY) => {
-    // clear the grid at the desired chords - at the moment just outputs the value of the chord
-    console.clear();
-    gridArr[chordX][chordY] = 1;
+// ask the user for desired grid coords to 'clear'
+rl.question('coords x: ', (chordX) => {
+    rl.question('coords y: ', (chordY) => {
     
+    console.clear();
+    // Assign the desired cell to 1, -1 from the user input so that the grid starts at 1,1 instead of 0,0
+    // This is only for the user expierence, the grid is still 0,0 based
+    gridArr[chordX - 1][chordY - 1] = 1;
+    
+    if(chordX < 1 || chordX > 3 || chordY < 1 || chordY > 3) {
+        console.log("Invalid coordinate, please enter a number between 1 and 3")
+    } else {
+    // Invoke the gridUpdate funciton to update the and re-log the grid
     gridUpdate();
+    }
+
+    //TODO - add a game over function
+    // if() {
+    //     gameOver = true;
+    // }
 
     // close the interface
     rl.close();
     });
 });
-
-
