b3793cb9e3b35447c4f6c2b72bc808f9c4ca604b
domitron123 <domtownshend@hotmail.com>
2023-04-26 07:21:59 +1200

    changes

--- recursion.js
+++ recursion.js
@@ -1,13 +1,14 @@
-function countDown(startNumber) {
-    if(startNumber < 1) {
-        return 1
-    }
-    // before recursion startNumber = 10
-    console.log("Before recursion startNumber " + startNumber)
-    countDown(startNumber - 1)
-    // after recursion startNumber = 1 
-    console.log("After recursion startNumber " + startNumber)
+// function countDown(startNumber) {
+//     if(startNumber < 1) {
+//         return 1
+//     }
+//     // before recursion startNumber = 10
+//     console.log("Before recursion startNumber " + startNumber)
+//     countDown(startNumber - 1)
+//     // after recursion startNumber = 1 
+//     console.log("After recursion startNumber " + startNumber)
     
-}
+// }
 
-countDown(10)
+// countDown(10)
+
