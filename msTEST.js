18100356a406a916608793ea4671698e3f6f9e97
domitron123 <dom-townshend@live.nmit.ac.nz>
2023-05-01 08:59:34 +1200

    reworking

--- msTEST.js
+++ msTEST.js
@@ -1,14 +1,18 @@
+// === MINESWEEPER GAME ===
+
 // import the readline module
 const readline = require('readline');
 
 // Initialize empty arrays and strings for grid generation
 let gridArr = [];
-let initClear = false;
+let initMine = true;
 let unclear = 0;
 let mine =  1;
-let clear = 2;
+let selected = false;
+
 gameOver = false;
 
+// call gridGen function with desired grid dimentions
 function gridGen(x, y) {
     // for each y value, push that many subarrays within gridArr !ROWS!
     for(let i = 0; i < y; i++) {
@@ -24,43 +28,42 @@ function gridGen(x, y) {
 
 // gridUpate function that converts the girdArr to a tileable string 'grid'
 function gridUpdate() {
+    console.clear()
 
-    // ANCHOR bomb generation
-    // if it's the first clear, then change a random index of gridArr to 1 to assign the bombs
-    if(initClear == false) {
-        // change a random index of gridArr to 1
-        gridArr[Math.floor(Math.random() * gridArr.length)][Math.floor(Math.random() * gridArr[0].length)] = mine;
-
-        initClear = true;
-    }
-
-    // Reset the grid string to nothing so it doesn't keep adding to the previous grid
+    // Reset / set the grid so it doesn't add to itself
+            //1:[0][0][0] (column numbers)
     let grid = "   1  2  3";
-              //1:[0][0][0] 
+            
     // loop for each subarray in gridArr (the rows)
     for(let i = 0; i < gridArr.length; i++) {
-        // if grid != "" then enter a new line 
+        // add a line break for each row
         if(grid != "") {
             grid += "\n"
         }
-        grid += i + 1 + ":"
+        // add the row number to the grid string
+        grid += i + 1 + ":"	
 
-        // for the length of each subarray (the colums) add a cell
+        // for each element in each subarray 
         for(let j = 0; j < gridArr[i].length; j++) {
+            
+            // update grid string in relation to gridArr board state
+            if(gridArr[i][j] == unclear || gridArr[i][j] == mine) {
+                grid += "[" + "x" + "]"
+            } else if(gridArr[i][j] == selected) {
+                grid += "[" + "A" + "]"
 
-            //ANCHOR - if the value of the cell is 0, add a 0 to the grid, else add a 1
-            if(gridArr[i][j] == unclear) {
-                grid += "[" + "-" + "]"; 
-                // SAFE CELL
-            } else if(gridArr[i][j] == clear) {
-                grid += "[" + " " + "]"; 
-                // BOMB CELL
-            } else if(gridArr[i][j] == mine) {
-                grid += "[" + "B" + "]"; 
             }
+
+            // old code for testing
+            // //(unclear = "[" + "-" + "]") : (clear = "[" + " " + "]") : (mine = "[" + "B" + "]")
+            // // grid = (gridArr[i][j] == unclear) ? grid += "[" + "-" + "]" : grid = grid;
+            // // grid = (gridArr[i][j] == clear) ? grid += "[" + " " + "]" : grid = grid;
+            // // grid = (gridArr[i][j] == mine) ? grid += "[" + "B" + "]" : grid = grid;
         }
     }
+    // log grid to the console (this is the game board)
     console.log(grid)
+    // invoke clearCell function to ask the user for input
     clearCell()
 }
 
@@ -71,53 +74,55 @@ const rl = readline.createInterface({
 });
 
 function clearCell() {
-    // ask the user for desired grid coords to 'clear'
+
+    if(initMine == true) {
+        // change a random index of gridArr to 1 (mine)
+        gridArr[Math.floor(Math.random() * gridArr.length)][Math.floor(Math.random() * gridArr[0].length)] = mine;
+        
+        initMine = false;
+    }
+    
+    console.log(gridArr) // debugging
+
+    // ask the user for desired grid coords to 'selected'
     rl.question('coords x: ', (coordX) => {
         rl.question('coords y: ', (coordY) => {
-        
-       
         
         // if the user input is not within the grid range return an error
         if(coordX < 1 || coordX > (gridArr.length)|| coordY < 1|| coordY > (gridArr[0].length)) {
             console.log("Invalid coordinate, please enter a number between 1 and " + gridArr[0].length + " for x and y")
         } else {
 
-        // Assign the desired cell to 1, -1 from the user input so that the grid starts at 1,1 instead of 0,0
+        // Assign the selected cell to 1, -1 from the user input so that the grid starts at 1,1 instead of 0,0
         // This is only for the user expierence, the grid is still 0,0 based
-        gridArr[coordY - 1][coordX - 1] = clear
-
-        
-        
-        }
-        // Invoke the gridUpdate funciton to update the and re-log the grid
-        gridUpdate()
-
-        // loop the functions if the game is not over
-        if(!gameOver) {
-            clearCell()
+        selected = gridArr[coordY - 1][coordX - 1];
         }
 
-        //TODO - add a game over function
-        // if() {
-        //     gameOver = true;
-        // }
-
-        // close the interface
-     
+        // if its the first time the user has selected a cell, then generate a mine
+        
+        
+        // if the selected cell is a mine, then game over
+        // !! I think this needs to check make sure the generated mine is not the same as the selected cell 
+        if(selected == mine) {
+            gameOver = true;
+            console.log("Game Over")
+            
+        } else {
+            // invoke gridUpdate function to update the grid and create gameloop
+            gridUpdate()
+            
+        }     
         });
     });
-    
 }
 
+
+// intially invoke functions to start the game
 gridGen(3, 3)
 gridUpdate()
 
 
-
-
-
-
-// FUNCTION PLANNING
+// FUNCTION PLANNING - POSSIBLY OUTDATED SINCE WRITTEN
 /*
 gridGen(x, y) 
 - create an array with subarrays equal to the grid dimentions. Possibly only needs to run once unless restarting the game
